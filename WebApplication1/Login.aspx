d9d3ee18f6d77eac3b79379e03c35ff361efc2d6
khaleedasafry <khaleedasafry@gmail.com>
2018-03-19 12:43:09 +0800

    initial commit

---
+++ WebApplication1/Login.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplication1.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">
+Login</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="heading" runat="server">
+    <asp:Image ID="imgMain" runat="server"  ImageUrl="/Images/logo2.jpg" alt="logo" style="width: 881px; height: 200px" />Unicorn Store</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="heading2" runat="server">
+Login Page</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="maincontent" runat="server">
+
+    <form id="form1" runat="server" style="text-align: center">
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Email:"></asp:Label>
+            <asp:TextBox ID="txtLoginEmail" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Password:"></asp:Label>
+            <asp:TextBox ID="txtLoginPassword" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+        </p>
+        <p>
+            <asp:Literal ID="litLoginError" runat="server"></asp:Literal>
+        </p>
+
+    </form>
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="GoogleMap" runat="server">
+</asp:Content>
---
+++ WebApplication1/Login.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+
+namespace WebApplication1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            var identityDbContext = new IdentityDbContext("IdentityConnectionString");
+            var userStore = new UserStore<IdentityUser>(identityDbContext);
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = userManager.Find(txtLoginEmail.Text, txtLoginPassword.Text);
+            if (user != null)
+            {
+                LogUserIn(userManager, user);
+            }
+            else
+            {
+                litLoginError.Text = "Invalid username or password";
+            }
+        }
+        private void LogUserIn(UserManager<IdentityUser> usermanager, IdentityUser user)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            var userIdentity = usermanager.CreateIdentity(
+             user, DefaultAuthenticationTypes.ApplicationCookie);
+        authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);             
+        }
+    }
+}
